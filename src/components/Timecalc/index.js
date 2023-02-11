25635b954299b270ce5a101dcd2c4c4f232f5744
jacksmedia <alex@j4cks.com>
2023-02-11 12:42:39 -0800

    :sparkles: count from first date shown

--- src/components/Timecalc/index.js
+++ src/components/Timecalc/index.js
@@ -2,12 +2,21 @@ import * as React from "react"
 
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 const Monat = new Date().toLocaleDateString('en-US', options )
-
+const date1 = new Date('5/12/2021')
+const date2 = new Date(Monat)
+const Difference_In_Time = date2.getTime() - date1.getTime()
+const Difference_In_Days = Difference_In_Time / (1000*60*60*24)
+const RoundedResult = Math.round(Difference_In_Days)
 
 const Timecalc = () => {
   return(
-    <div>⚓️ Today is
-      <span> {Monat}</span>
+    <div>
+      <div>⚓️ Today is
+        <span> {Monat}</span>
+      </div>
+      <div>🤯 It's been
+        <span> {RoundedResult} since this practice began.</span>
+      </div>
     </div>
   )
 }
