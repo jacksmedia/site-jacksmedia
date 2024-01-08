9d62fa2485f06f605891a6a25c4bc4fa1d906250
m340857 <emilye.brown@gmail.com>
2024-01-07 17:47:32 -0800

    Color update, edit

--- src/components/Timecalc/index.jsx
+++ src/components/Timecalc/index.jsx
@@ -23,35 +23,35 @@ const warmUps = [
     "title":"Yoga For Neck, Shoulders, Upper Back",
     "length":"10m",
     "seconds":600,
-    "bgc":"cornflowerblue"
+    "bgc":"color3"
   },
   {
     "url":"https://youtu.be/50txkOrJn9o?si=LWi3_8yKID6I523K&t=88",
     "emojis":"🦩🪃","title":"Tai Chi 5 Min a Day Warm Up",
     "length":"8m24s",
     "seconds":504,
-    "bgc":"lemonchiffon"
+    "bgc":"color1"
     },
   {
     "url":"https://youtu.be/SedzswEwpPw?si=W12HalSuXWHUrjlE&t=52",
     "emojis":"🧣🎒","title":"Yoga for Neck & Shoulder Relief",
     "length":"16m6s",
     "seconds":966,
-    "bgc":"cornflowerblue"
+    "bgc":"color3"
     },
   {
     "url":"https://youtu.be/d-PTjUgMrig?si=-0yAPMV8tY_WJqDd&t=78",
     "emojis":"☀️🌐","title":"Sun & Earth Purification Qigong",
     "length":"7m57s",
     "seconds": 477,
-    "bgc":"mediumaquamarine"
+    "bgc":"color2"
     },
   {
     "url":"https://youtu.be/jOfshreyu4w?si=Aq06Cg3LYrvCDT19&t=39",
     "emojis":"🫂🏅","title":"Quick Stress Fix 5 Min Sequence",
     "length":"4m44s",
     "seconds": 284,
-    "bgc":"cornflowerblue"
+    "bgc":"color3"
   }
 ]
 const exercises = [
@@ -60,98 +60,98 @@ const exercises = [
     "emojis":"💨🍯","title":"Qigong to Purge & Tonify",
     "length":"18m37s",
     "seconds": 1117,
-    "bgc":"mediumaquamarine"
+    "bgc":"color2"
     },
   {
     "url":"https://youtu.be/cEOS2zoyQw4?si=EApSfyPYr5WgxM9y&t=91",
     "emojis":"🦆🌊","title":"Tai Chi 5 Min a Day Module 01",
     "length":"4m23s",
     "seconds": 263,
-    "bgc":"lemonchiffon"
+    "bgc":"color1"
     },
   {
     "url":"https://youtu.be/HMbT-CPVl2k?si=RvLYoxjStRVeaYUO&t=48",
     "emojis":"🍵🐵","title":"Mood Lifter",
     "length":"14m37s",
     "seconds": 877,
-    "bgc":"mediumaquamarine"
+    "bgc":"color2"
     },
   {
     "url":"https://youtu.be/enk0bOv-gF8?si=kooRHEd1XzdpAfr_&t=18",
     "emojis":"🦇☁️","title":"Tai Chi 5 Min a Day Module 02",
     "length":"5m42s",
     "seconds": 342,
-    "bgc":"lemonchiffon"
+    "bgc":"color1"
     },
   {
     "url":"https://youtu.be/8wddPSIEpvE?si=5M0u1_wcRtYs6uaV&t=77",
     "emojis":"🐠🛸","title":"Qigong: Opening the Door of Life",
     "length":"11m43s",
     "seconds": 703,
-    "bgc":"mediumaquamarine"
+    "bgc":"color2"
     },
   {
     "url":"https://youtu.be/OPcZlXYcdMA?si=Q0S5rRdkRcNaLkGy&t=31",
     "emojis":"🐎🌕","title":"Tai Chi 5 Min a Day Module 03",
     "length":"5m16s",
     "seconds": 316,
-    "bgc":"lemonchiffon"
+    "bgc":"color1"
     },
   {
     "url":"https://youtu.be/IyINAjEoTIs?si=nF8uPnAU6t0WVe4w&t=103",
     "emojis":"🚪🦅","title":"Qigong for Beginners",
     "length":"28m4s",
     "seconds": 1684,
-    "bgc":"mediumaquamarine"
+    "bgc":"color2"
     },
   {
     "url":"https://youtu.be/RoIqYtiTLFI?si=fX__f4j_ygWQ4ByO&t=18",
     "emojis":"🐍🍂","title":"Tai Chi 5 Min a Day Module 04",
     "length":"9m39s",
     "seconds": 579,
-    "bgc":"lemonchiffon"
+    "bgc":"color1"
     },
   {
     "url":"https://youtu.be/nmmNWj9YtAw?si=ZIxziO8gm3vx8ffQ&t=62",
     "emojis":"🐯🌋","title":"Daily Qiqong Routine",
     "length":"15m50s",
     "seconds": 950,
-    "bgc":"mediumaquamarine"
+    "bgc":"color2"
     },
   {
     "url":"https://youtu.be/62a20CiIAlY?si=bV2k3YkLIjfHTcWe&t=45",
     "emojis":"🐉☂️","title":"Tai Chi 5 Min a Day Module 05",
     "length":"6m4s",
     "seconds": 364,
-    "bgc":"lemonchiffon"
+    "bgc":"color1"
     },
   {
     "url":"https://youtu.be/EZT8RC0wRbA?si=1u4ZkyICs-RFCiAh&t=113",
     "emojis":"🐻🔥","title":"Qigong: Circulate Your Energy",
     "length":"13m24s",
     "seconds": 804,
-    "bgc":"mediumaquamarine"
+    "bgc":"color2"
     },
   {
     "url":"https://youtu.be/tbwbL1pg0HY?si=7XtAtjl2eZaBEf3g&t=30",
     "emojis":"🐒🦜","title":"Seated Tai Chi for Home Practice",
     "length":"18m",
     "seconds": 1080,
-    "bgc":"indianred"
+    "bgc":"color4"
     },
   {
     "url":"https://youtu.be/Ba0fweKUwIc?si=LljtiMSWdmXoi54_&t=37",
     "emojis":"🌬🦮","title":"Yoga to Calm Your Nerves",
     "length":"23m35s",
     "seconds": 1415,
-    "bgc":"cornflowerblue"
+    "bgc":"color3"
     },
   {
     "url":"https://youtu.be/_V29hE0_oBE?si=GiDMPoCu9RynHwNj&t=47",
     "emojis":"🦉🌜","title":"Seated Tai Chi",
     "length":"10m6s",
     "seconds": 606,
-    "bgc":"indianred"
+    "bgc":"color4"
   }
 ]
 const extras = [
@@ -160,42 +160,42 @@ const extras = [
     "emojis":"🐉🌀","title":"Swimming Dragon",
     "length":"8m28s",
     "seconds": 508,
-    "bgc":"mediumaquamarine"
+    "bgc":"color2"
     },
   {
     "url":"https://youtu.be/Yzm3fA2HhkQ?si=9U-udKw3-FJ9eVZ7&t=50",
     "emojis":"🪢🪡","title":"Yoga For Flexibility",
     "length":"15m4s",
     "seconds": 904,
-    "bgc":"cornflowerblue"
+    "bgc":"color3"
     },
   {
     "url":"https://www.youtube.com/watch?v=V1HbXt5ZRlg&t=39s",
     "emojis":"🗼🪜","title":"Head Stand Yoga Pose",
     "length":"17m59s",
     "seconds": 1079,
-    "bgc":"cornflowerblue"
+    "bgc":"color3"
     },
   {
     "url":"https://youtu.be/ED3_i_xVd_s?si=PEn7R0kCBX905vKR&t=56",
     "emojis":"🎱🌈","title":"Qigong to Manage Stress & Anxiety",
     "length":"9m2s",
     "seconds": 542,
-    "bgc":"lemonchiffon"
+    "bgc":"color1"
     },
   {
     "url":"https://youtu.be/FgfT2fOv31E?si=WO9tE46rSLPDiuQr&t=40",
     "emojis":"🪗🫧","title":"Yoga For Abs",
     "length":"6m21s",
     "seconds": 381,
-    "bgc":"cornflowerblue"
+    "bgc":"color3"
     },
   {
     "url":"https://youtu.be/LZ2oHU-mMJI?si=atFLf7MT6ksObXlV&t=443",
     "emojis":"🕸🎡","title":"Spinning the Silk Wheel",
     "length":"4m58s",
     "seconds": 298,
-    "bgc":"lemonchiffon"
+    "bgc":"color1"
     },
   {
     "url":"https://youtu.be/Nnd5Slo02us?si=9gwhxzB009KkaALo&t=26",
@@ -203,21 +203,21 @@ const extras = [
     "length":"9m10s",
     "seconds": 550
 ,
-    "bgc":"cornflowerblue"
+    "bgc":"color3"
     },
   {
     "url":"https://youtu.be/eBdfCX5XnX4?si=-0EK8KmKW8tXCkul&t=5",
     "emojis":"4️⃣🦎","title":"Yoga For Hips",
     "length":"5m57s",
     "seconds": 357,
-    "bgc":"cornflowerblue"
+    "bgc":"color3"
     },
   {
     "url":"https://youtu.be/8T39OBNaNzU?si=tks83fkkQcGWN02p&t=27",
     "emojis":"✂🧱","title":"Core Conditioning",
     "length":"11m11s",
     "seconds": 671,
-    "bgc":"cornflowerblue"
+    "bgc":"color3"
   }
 ]
 const howManyWarmUps = warmUps.length
