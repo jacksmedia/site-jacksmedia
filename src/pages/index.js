10908438a0b8283915bef2b0fb9eb552869ee572
Jacks Media <73559558+jacksmedia@users.noreply.github.com>
2025-11-05 12:56:44 -0800

    🔗Update link to redirect to new URL

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="https://j4cks-flux.onrender.com/">
+            to="https://www.j4cks.com/">
             Go To App
           </Link>
         </div>
